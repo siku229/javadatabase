99775269e580834a510860596348a0a73666ce46
siku <jths0229@gmail.com>
2022-03-30 09:36:21 +0900

    실험용

--- src/sql/0329.sql
+++ src/sql/0329.sql
@@ -15,4 +15,4 @@ insert into aaa values ('홍길동',25,default,default);
 insert into aaa values ('홍길순',29,'f',default);
 insert into aaa values ('김말숙',35,'f','2020-01-05');
 
-select * from aaa;
+--select * from aaa; 실험용
